06f0d471c2262a6b34ccfd82e467be2445eb4e6a
gastonmartinezz <ezegaston2011@gmail.com>
2023-10-04 20:39:02 -0300

    no se

--- js/index.js
+++ js/index.js
@@ -1,77 +1,70 @@
-const url = `https://japceibal.github.io/japflix_api/movies-data.json`;
-const btn = document.getElementById('inputBuscar')
-const contenedor = document.getElementById('catalogo')
+/* const url = `https://japceibal.github.io/japflix_api/movies-data.json`;
+const btn = document.getElementById('inputBuscar');
 
-
-document.addEventListener('DOMContentLoaded',()=> {
-  
+function cargar(btnValue) {
   try{
     fetch(url)
-    .then(response =>response.json())
+    .then(response => response.json())
     .then(data => {
+      data.forEach(function(movie) {
+        const ul = document.getElementById('lista');
+        ul.innerHTML = '';
 
-      items.forEach((item) => {
-        const li = document.createElement("li");
-        li.textContent = item;
-        li.className = "list-group-item";
-        itemList.appendChild(li);
-      });
+        if (movie.title.toLowerCase().includes(btnValue.toLowerCase()) || 
+            movie.tagline.toLowerCase().includes(btnValue.toLowerCase()) ||
+            movie.genres.toLowerCase().includes(btnValue.toLowerCase()) ||
+            movie.overview.toLowerCase().includes(btnValue.toLowerCase())) {
 
+          const estrellas = '<i class="fa fa-star"></i>'.repeat(movie.vote_average/2);
+          const li = document.createElement("li");
+
+          li.className = ""
+          li.innerHTML = 
+          `
+            <div class="top">
+              <h4> ${movie.title} </h4>
+              <p> ${estrellas} <p>
+            </div>
+            <p class="desc">${movie.tagline}</p>
+          `
+          ul.appendChild(li);
+        }
+      })
     })
+  } catch (error) {
+    console.log(error);
   }
-  catch(error){
-    console.log(error)
-  }
-  })
+}
+const btnBuscar = document.getElementById('btnBuscar');
 
-  
+btnBuscar.addEventListener('click', () => {
+  const btnValue = btn.value;
+  cargar(btnValue);
+}) */
 
-
-
-
-
-
-
-
-  
- /*  function crearLista(registro){
-
-    for (const i of registro){
-      contenedor.innerHTML +=
-       
-      `
-          <ul>
-            <li class="titulo buscador"> ${i.title} </li>
-            <li class ="descripcion buscador"> ${i.tagline} </li>
-            <li class ="sold"> Unidades vendidas: ${i.vote_average}</li>
-          </ul>
-        
-      `
+async function searchMovies() {
+  const searchTerm = document.getElementById('inputBuscar').value;
+  const response = await fetch('https://japceibal.github.io/japflix_api/movies-data.json');
+  const movies = await response.json();
+  const listaDiv = document.getElementById('lista');
+  listaDiv.innerHTML = '';
+  movies.forEach(movie => {
+    if (movie.title.includes(searchTerm) || movie.genres.includes(searchTerm) || movie.tagline.includes(searchTerm) || movie.overview.includes(searchTerm)) {
+      const movieDiv = document.createElement('div');
+      movieDiv.innerHTML = `<p><strong>${movie.title}</strong> - ${movie.tagline} - Vote Average: ${movie.vote_average}</p>`;
+      movieDiv.addEventListener('click', () => showMovieDetails(movie));
+      listaDiv.appendChild(movieDiv);
     }
-  };
- */
+  });
+}
 
-
-
-/* function buscar(e) {
-  if (e.target.matches('inputBuscar')) {
-      
-    let searchText = e.target.value.toLowerCase();
-    let fichasTexto = document.querySelectorAll("div.descDeArticulo");
-
-    fichasTexto.forEach(producto => {
-      let fichaTitulo = producto.getElementsByClassName("titulo")[0].innerHTML.toLowerCase();
-      let fichaDesc = producto.getElementsByClassName("descripcion")[0].innerHTML.toLowerCase();
-      
-      const fichaEntera = producto.closest('.articulo');
-
-      if (fichaTitulo.includes(searchText) || fichaDesc.includes(searchText)) {
-        // Musestra el elemento si coincide con la búsqueda
-        fichaEntera.style.display = 'block';
-      } else {
-        // Oculta el elemento si no coincide con la búsqueda
-        fichaEntera.style.display = 'none';
-      }
-    })
-  }
-} */
+function showMovieDetails(movie) {
+  const detailTitle = document.getElementById('detailTitle');
+  const detailOverview = document.getElementById('detailOverview');
+  const detailGenres = document.getElementById('detailGenres');
+  detailTitle.innerText = movie.title;
+  detailOverview.innerText = movie.overview;
+  detailGenres.innerText = 'Genres: ' + movie.genres.join(', ');
+  const movieDetailsDiv = document.getElementById('movieDetails');
+  movieDetailsDiv.style.display = 'block';
+}
